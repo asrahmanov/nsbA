20c6adf20a3051457989dea5e986e821456eb79d
asrahmanov <asrahmanvo@gmail.com>
2022-08-02 19:14:23 +0300

    cron mail

--- socket/cron.js
+++ socket/cron.js
@@ -246,9 +246,12 @@ async function main(textMail, email, subject, id, replyTo, action, attachments =
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-            user: "crm@crm.i-bios.com", // generated ethereal user
+            // user: "crm@crm.i-bios.com", // generated ethereal user
+            // pass: "t*poYagKI1I2" // generated ethereal password
+            user: "crm@nbs-platforms.ru", // generated ethereal user
             pass: "frerfre2343y45y36453g" // generated ethereal password
         }
+
     });
 
 
@@ -257,7 +260,8 @@ async function main(textMail, email, subject, id, replyTo, action, attachments =
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: 'crm@crm.i-bios.com', // sender address
+        // from: 'crm@crm.i-bios.com', // sender address
+        from: 'crm@nbs-platforms.ru', // sender address
         to: email, // list of receivers
         replyTo: replyTo, // list of receivers
         subject: subject, // Subject line
